fdaa3154c3ba387b1dae21912c8825d467857b7b
Igor <igorek20045565@gmail.com>
2018-08-07 13:53:26 +0300

    Add pypy

--- setup.py
+++ setup.py
@@ -1,30 +1,32 @@
 from setuptools import setup
 import qiwi_api
 
-with open("README.md", "r", encoding='utf-8') as f:
+with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name="qiwi_api",
+    name='qiwi_api',
     version=qiwi_api.__version__,
 
-    author="Helow19274",
-    author_email="helow@helow19274.tk",
+    author='Helow19274',
+    author_email='helow@helow19274.tk',
 
-    description="Qiwi api wrapper",
+    description='Qiwi api wrapper',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
 
-    url="https://github.com/helow19274/qiwi_api",
+    url='https://github.com/helow19274/qiwi_api',
     packages=['qiwi_api'],
     install_requires=['requests'],
 
     classifiers=(
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7"
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
     )
 )
